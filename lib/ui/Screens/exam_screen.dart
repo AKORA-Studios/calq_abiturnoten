2677be95809ef326c79c1e3df14fbd66e24647b0
Kiara <frida.shivaa@gmail.com>
2025-04-02 16:59:12 +0200

    add block views

--- lib/ui/Screens/exam_screen.dart
+++ lib/ui/Screens/exam_screen.dart
@@ -8,25 +8,82 @@ class ExamScreen extends StatefulWidget {
 }
 
 class _ExamScreenState extends State<ExamScreen> {
-  List<double> points = [0, 0, 0, 0, 0];
+  List<double> _points = [0, 0, 0, 0, 0];
+  double _block1Value = 0.5;
+  double _block2Value = 0.3;
 
   Widget examView(int i) {
     return Column(children: [
-      Text("Exam $i [${points[i].toInt()}]"),
+      Text("Exam $i [${_points[i].toInt()}]"),
       Slider(
         //  activeColor: widget.sub.color, // TODO:
         min: 0,
-        label: '${points[i].round()}',
+        label: '${_points[i].round()}',
         divisions: 15,
         max: 15,
-        value: points[i],
+        value: _points[i],
         onChanged: (value) {
           setState(() {
-            points[i] = value;
+            _points[i] = value;
           });
         },
       )
     ]);
+  }
+
+  Widget blockView() {
+    return Card(
+        child: Padding(
+      padding: EdgeInsets.all(8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Row(
+            children: [
+              Expanded(
+                child: Text("Block 1"),
+                flex: 2,
+              ),
+              Expanded(
+                child: Text("Block 2"),
+                flex: 1,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: LinearProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.green),
+                  value: _block1Value,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: LinearProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.green),
+                  value: _block2Value,
+                ),
+              ),
+            ],
+          ),
+          const Row(
+            children: [
+              //    LinearProgressIndicator(),
+              Expanded(
+                child: Text("? von 600"),
+                flex: 2,
+              ),
+              Expanded(
+                child: Text("? von 300"),
+                flex: 1,
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
   }
 
   @override
@@ -38,7 +95,13 @@ class _ExamScreenState extends State<ExamScreen> {
         ),
         body: SingleChildScrollView(
           child: Column(
-            children: [0, 1, 2, 3, 4].map((i) => examView(i)).toList(),
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: blockView(),
+              ),
+              ...[0, 1, 2, 3, 4].map((i) => examView(i)).toList()
+            ],
           ),
         ));
   }
